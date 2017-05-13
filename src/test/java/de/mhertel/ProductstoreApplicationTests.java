96ea77fc1910d8afc2c535e4081ef53ffab29905
Matthias Hertel <me@matthiashertel.eu>
2017-05-13 12:44:01 +0200

    initial

---
+++ src/test/java/de/mhertel/ProductstoreApplicationTests.java
@@ -0,0 +1,16 @@
+package de.mhertel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductstoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
